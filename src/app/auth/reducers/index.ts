e10c7886bea10850c883c53d613a07a62bba057b
Sandy Nelson <sandy.nelson@nexius.hu>
2021-04-20 16:18:09 +0200

    Rename state

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -6,10 +6,10 @@ import {
   MetaReducer
 } from '@ngrx/store';
 
-export interface State {
+export interface AppState {
 
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const reducers: ActionReducerMap<AppState> = {
 
 };
